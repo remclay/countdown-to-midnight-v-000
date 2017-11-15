01e8f9ce2c3fa3c14cdaf9c44b6a9712ef165e43
Remmy Clay <remmy.lauren@gmail.com>
2017-11-15 03:06:29 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -8,8 +8,6 @@ def countdown(number)
   puts "HAPPY NEW YEAR!"
 end
 
-countdown(3)
-
 def countdown_with_sleep(number)
   while number > 0
     puts "#{number} SECOND(S)!"
@@ -18,3 +16,5 @@ def countdown_with_sleep(number)
   end
   puts "HAPPY NEW YEAR!"
 end
+
+countdown_with_sleep(3)
